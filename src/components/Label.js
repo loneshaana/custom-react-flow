2c825f7086dce0483cc898e188e0a1bc6532325b
shaan <anwarulhaq794@gmail.com>
2020-01-24 10:45:31 +0530

    Create Label.js

---
+++ src/components/Label.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Label = styled.div`
+  color: #b4886b;
+  font-weight: bold;
+  display: block;
+  margin-top: 1em;
+  margin-bottom: 1em;
+`;

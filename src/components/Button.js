71f6b120cca18069f3f7ae73f1bf148a72d25750
shaan <anwarulhaq794@gmail.com>
2020-01-24 10:45:17 +0530

    Create Button.js

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Button = styled.button`
+  /* This renders the buttons above... Edit me! */
+  display: inline-block;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  padding: 0.7rem 0;
+  margin: 0.7rem 1rem;
+  width: 11rem;
+  background: palevioletred;
+  color: white;
+  font: 22px;
+`;
